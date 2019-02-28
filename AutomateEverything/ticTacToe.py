f9c13c82a9168ce0764245e70b1325316a674811
Drtrider <drtrider200@gmail.com>
2019-02-28 08:27:48 -0800

    Worked on some of the book conecepts. Moved them into their own folder.

---
+++ AutomateEverything/ticTacToe.py
@@ -0,0 +1,9 @@
+# Guide provided by https://www.youtube.com/watch?v=tf3ezjeTpfI&list=PLQVvvaa0QuDeAams7fkdcwOGBpGdHpXln&index=3
+
+game = [[0, 0, 0,],
+        [0, 0, 0,],
+        [0, 0, 0,],]
+
+print (" a  b  c")
+for count, row in enumerate(game):
+    print(count, row)
