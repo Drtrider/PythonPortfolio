c38fbc70418bb4a341640c9fc1513bf5e546a2e8
Drtrider <drtrider200@gmail.com>
2024-10-27 08:03:37 -0700

    Cleaning up old stuff.

--- NFL_Thing/nfl_data_fetcher.py
+++ NFL_Thing/nfl_data_fetcher.py
@@ -4,11 +4,23 @@ Purpose of nfl_data_fetcher.py is to be able to fetch some player data.
 PRE-REQ: You'll need to generate a roster.json using NFL_Thing\roster.py
 """
 
+#######################
+# Still to do
+#######################
+
+# Deliverable #1: Suggest a list of players for the initial draft
+
+# Deliverable #2: Suggest players for each week
+
+################################
+################################
+
+
 import pandas as pd
 import json
 
 
-def load_stats_dataframe(path_to_json):
+def create_stats_dataframe(path_to_json):
     """
     Loads a .json player stats file into a pandas DataFrame.
 
@@ -47,9 +59,29 @@ def load_stats_dataframe(path_to_json):
 
     return df_player_stats
 
+def create_df_of_players(path_to_player_list):
+    """Returns a dataframe of players, if given a roster.json"""
 
-# Pick which players to lookup
+    # Load the JSON data from a file
+    with open(path_to_player_list) as f:
+        data = json.load(f)
 
-# Lookup batch of players player's stats
+    # Create the dataframe
+    df_players = pd.json_normalize(data)
 
-# Return some type of reccomendation on which players to pick? 
+    return df_players
+
+# Create a df of players
+path_to_player_list = r"E:\Documents\Repos\PythonPortfolio-1\NFL_Thing\data\roster.json"
+df_players = create_df_of_players(path_to_player_list)
+
+# Create a dataframe of stats for first 10  players
+pd.DataFrame()
+for player in df_players.head(10).itertuples():
+    print(f"Looking up stats for: {player.fullName}")
+
+
+
+
+
+
